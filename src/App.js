8410578a2201b89ef99ffd98796409c5c3e643d4
Florian Sloot <flosloot@gmx.de>
2020-06-07 16:57:18 +0200

    [STACKERS] test stage deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from the blue CI/CD! III</p>
+        <p>Hello from the blue CI/CD! Stage Branch!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
